4140b5aa04afd4570bb7de1de7d358a0115a658f
Amal <amalfer0308@gmail.com>
2025-04-13 18:18:10 +0530

    update on images

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -474,7 +474,9 @@ const Index = () => {
                     <div className="absolute inset-0 bg-gradient-to-br from-strategic-green/5 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
                     <div className="relative z-10">
                       <Image
-                        src={`/industries/${index + 1}.${industry.type}`}
+                        src={getImagePath(
+                          `industries/${index + 1}.${industry.type}`
+                        )}
                         alt={industry.name}
                         className="h-40 w-full object-cover rounded-lg mb-4 group-hover:scale-105 transition-transform duration-300"
                         placeholderClassName="h-40"
